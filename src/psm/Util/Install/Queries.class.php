phpservermon/phpservermon

commit 7a91b8a3f3143248c767d576505ad9239582b975

Add database functions and upgrade queries for uptime logging feature

src/psm/Util/Install/Queries.class.php
@@ -130,6 +130,16 @@ class Queries {
 
 
 			}
+            if(version_compare($version_from, '2.1.0', '<=')) {
+                // 2.1 upgrade
+                $queries[] = "CREATE TABLE  `" . PSM_DB_PREFIX . "uptime` (
+                        `server_id` INT( 11 ) NOT NULL ,
+                        `date` DATETIME NOT NULL ,
+                        `status` INT( 1 ) NOT NULL ,
+                        `latency`  FLOAT( 9, 7 ) NULL
+                       ) ENGINE = MYISAM ;"
+            }
+        }
 			$queries[] = "UPDATE `" . PSM_DB_PREFIX . "config` SET `value` = '{$version}' WHERE `key` = 'version';";
 		}
 		return $queries;

src/includes/functions.inc.php
@@ -169,6 +169,26 @@ function psm_add_log($server_id, $type, $message, $user_id = null) {
 }
 
 /**
+ * This function adds the result of a check to the uptime table for logging purposes.
+ *
+ * @param int $server_id
+ * @param string $message
+ */
+function psm_log_uptime($server_id, $status, $latency) {
+    global $db;
+
+    $db->save(
+        PSM_DB_PREFIX.'uptime',
+        array(
+            'server_id' => $server_id,
+            'date' => date('Y-m-d H:i:s'),
+            'status' => $status,
+            'latency' => $latency,
+        )
+    );
+}
+
+/**
  * Parses a string from the language file with the correct variables replaced in the message
  *
  * @param string $status is either 'on' or 'off'

commit 32a6304f804bdff5b6eb3246d6303b79fcadc332

fixing fatal error introduced by commit 7a91b8a3f3143248c767d576505ad9239582b975 (missing semicolon & extra closing tag)

src/psm/Util/Install/Queries.class.php
@@ -137,9 +137,8 @@ class Queries {
                         `date` DATETIME NOT NULL ,
                         `status` INT( 1 ) NOT NULL ,
                         `latency`  FLOAT( 9, 7 ) NULL
-                       ) ENGINE = MYISAM ;"
+                       ) ENGINE = MYISAM ;";
             }
-        }
 			$queries[] = "UPDATE `" . PSM_DB_PREFIX . "config` SET `value` = '{$version}' WHERE `key` = 'version';";
 		}
 		return $queries;