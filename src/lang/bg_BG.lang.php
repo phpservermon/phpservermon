1fa5f3e98d227c9d5ac9d8f3825bd75425646a76
Plamen Vasilev <p.vasileff@gmail.com>
2014-04-24 14:43:08 +0300

    Update bg_BG.lang.php

--- src/lang/bg_BG.lang.php
+++ src/lang/bg_BG.lang.php
@@ -171,6 +171,7 @@ $sm_lang = array(
 		'sms_gateway_spryng' => 'Spryng',
 		'sms_gateway_inetworx' => 'Inetworx',
 		'sms_gateway_clickatell' => 'Clickatell',
+		'sms_gateway_smsit' => 'Smsit',
 		'sms_gateway_textmarketer' => 'Textmarketer',
 		'sms_gateway_username' => 'Потребител',
 		'sms_gateway_password' => 'Парола',
