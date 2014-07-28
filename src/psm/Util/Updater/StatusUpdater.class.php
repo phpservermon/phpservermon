phpservermon/phpservermon

commit 4a09d9ec22fdcc75dcc60b2438fbb820b707e375

adding if is_resource check around fclose to prevent warnings

src/psm/Util/Updater/StatusUpdater.class.php
@@ -149,7 +149,9 @@ class StatusUpdater {
 		$status = ($fp === false) ? false : true;
 		$this->rtime = (microtime(true) - $starttime);
 
-		fclose($fp);
+		if(is_resource) {
+			fclose($fp);
+		}
 
 		// check if server is available and rerun if asked.
 		if(!$status && $run < $max_runs) {

commit b79fbafe4735638bb63f962cd91f5ab52e0fb11b

fixing wrong is_resource call (introduced by 4a09d9ec22fdcc75dcc60b2438fbb820b707e375)

src/psm/Util/Updater/StatusUpdater.class.php
@@ -149,7 +149,7 @@ class StatusUpdater {
 		$status = ($fp === false) ? false : true;
 		$this->rtime = (microtime(true) - $starttime);
 
-		if(is_resource) {
+		if(is_resource($fp)) {
 			fclose($fp);
 		}
 
@@ -180,7 +180,7 @@ class StatusUpdater {
 		);
 
 		$this->rtime = (microtime(true) - $starttime);
-		
+
 		// the first line would be the status code..
 		$status_code = strtok($curl_result, "\r\n");
 		// keep it general