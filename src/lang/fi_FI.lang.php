53ee67773e28901274bdcbdb22b9f226641f33f4
Sami Nieminen <nieminen.sami2@gmail.com>
2015-06-08 12:13:44 +0000

    Translation done, testing begins...

--- src/lang/fi_FI.lang.php
+++ src/lang/fi_FI.lang.php
@@ -79,7 +79,7 @@ $sm_lang = array(
 		'level_10' => 'Järjestelmänvalvoja',
 		'level_20' => 'Käyttäjä',
 		'level_description' => '<b>Järjestelmänvalvojilla</b> on täydet oikeudet: he voivat hallita palvelimia, käyttäjiä ja muokata ohjelmiston asetuksia.<br/><b>Käyttäjät</b> voivat vain nähdä ja päivittää palvelimia jotka on asetettu heille.',
-		'mobile' => 'Mobiili',
+		'mobile' => 'Puhelin',
 		'email' => 'Sähköposti',
 		'pushover' => 'Pushover',
 		'pushover_description' => 'Pushover on palvelu jolla on helppo lähettää reaaliaikaisia tilaviestejä. Katso <a href="https://pushover.net/">verkkosivuilta</a> lisäinfoa.',
@@ -206,22 +206,22 @@ $sm_lang = array(
 		'pushover_api_token_description' => 'Ennen kuin voit käyttää Pushoveria, sinun täytyy <a href="%1$s" target="_blank">rekisteröidä sovellus</a> heidän nettisivuillaan, ja kopioida API-avain tänne.',
 		'alert_type' => 'Valitse milloin haluat ilmoituksia.',
         'alert_type_description' => '<b>Status change:</b> '.
-		    'You will receive a notifcation when a server has a change in status. So from online -> offline or offline -> online.<br/>'.
-		    '<br /><b>Offline:</b> '.
-		    'You will receive a notification when a server goes offline for the *FIRST TIME ONLY*. For example, '.
-		    'your cronjob is every 15 mins and your server goes down at 1 am and stays down till 6 am. '.
-		    'You will get 1 notification at 1 am and thats it.<br/>'.
-		    '<br><b>Always:</b> '.
-		    'You will receive a notification every time the script runs and a site is down, even if the site has been offline for hours.',
+		    'Saat ilmoituksen kun palvelimen tila vaihtuu. Eli tilasta päällä -> sammunut tai sammunut -> päällä.<br/>'.
+		    '<br /><b>Sammunut:</b> '.
+		    'Saat yhden ilmoituksen kun palvelimen tila vaihtuu sammuneeksi, mutta *VAIN ENSIMMÄISEN KERRAN*. Esimerkiksi, '.
+		    'jos tarkistus tehdään joka 15 minuutti, ja palvelin sammuu klo 1 ja pysyy sammuneena klo 6 asti. '.
+		    'Saat vain yhden ilmoituksen klo 1, ei muuta.<br/>'.
+		    '<br><b>Aina:</b> '.
+		    'Saat ilmoituksen joka kerta kun palvelin tarkistetaan, vaikka palvelin olisi ollut sammuneena tunteja.',
 		'alert_type_status' => 'Tilan muutos',
 		'alert_type_offline' => 'Sammunut',
 		'alert_type_always' => 'Aina',
-		'log_status' => 'Log status',
-		'log_status_description' => 'If log status is set to TRUE, the monitor will log the event whenever the Notification settings are passed.',
-		'log_email' => 'Log emails sent by the script',
-		'log_sms' => 'Log text messages sent by the script',
-		'log_pushover' => 'Log pushover messages sent by the script',
-		'updated' => 'The configuration has been updated.',
+		'log_status' => 'Merkitse tapahtumat',
+		'log_status_description' => 'Jos arvo on tosi, palvelin tallentaa tapahtumamerkinnän jokaisesta tilan muutoksesta.',
+		'log_email' => 'Tallenna lähetetyt sähköpostitapahtumat',
+		'log_sms' => 'Tallenna lähetetyt tekstiviestitapahtumat',
+		'log_pushover' => 'Tallenna lähetetyt Pushover-tapahtumat',
+		'updated' => 'Asetukset tallennettu ja päivitetty.',
 		'tab_email' => 'Sähköposti',
 		'tab_sms' => 'Tekstiviesti',
 		'tab_pushover' => 'Pushover',
@@ -229,18 +229,18 @@ $sm_lang = array(
 		'settings_sms' => 'Tekstiviesti asetukset',
 		'settings_pushover' => 'Pushover asetukset',
 		'settings_notification' => 'Ilmoitusasetukset',
-		'settings_log' => 'Log settings',
-		'auto_refresh' => 'Auto-refresh',
+		'settings_log' => 'Tapahtumamerkintöjen asetukset',
+		'auto_refresh' => 'Automaattipäivitys',
 		'auto_refresh_servers' =>
-			'Auto-refresh servers page.<br/>'.
+			'Päivittää automaattisesti palvelimet-sivun.<br/>'.
 			'<span class="small">'.
-			'Time in seconds, if 0 the page won\'t refresh.'.
+			'Aika sekunteina, jos 0, sivu ei päivity automaattisesti.'.
 			'</span>',
 		'seconds' => 'sekuntia',
 		'test' => 'Testi',
-		'test_email' => 'An email will be sent to the address specified in your user profile.',
-		'test_sms' => 'An SMS will be sent to the phone number specified in your user profile.',
-		'test_pushover' => 'A Pushover notification will be sent to the user key/device specified in your user profile.',
+		'test_email' => 'Testisähköposti lähetetään profiilisi sähköpostiosoitteeseen.',
+		'test_sms' => 'Testitekstiviesti lähetetään profiilisi numeroon.',
+		'test_pushover' => 'Pushover-ilmoitus lähetetään profiilissa asetettuun laitteeseen.',
 		'send' => 'Lähetä',
 		'test_subject' => 'Testi',
 		'test_message' => 'Testiviesti',
@@ -248,27 +248,27 @@ $sm_lang = array(
 		'email_error' => 'Virhe sähköpostin lähetyksessä',
 		'sms_sent' => 'Tekstiviesti lähetetty',
 		'sms_error' => 'Virhe tekstiviestin lähetyksessä',
-		'sms_error_nomobile' => 'Unable to send test SMS: no valid phone number found in your profile.',
+		'sms_error_nomobile' => 'Testitektiviestin lähetys epäonnistui: toimivaa numeroa ei löydetty profiilistasi.',
 		'pushover_sent' => 'Pushover ilmoitus lähetetty',
-		'pushover_error' => 'An error has occurred while sending the Pushover notification: %s',
-		'pushover_error_noapp' => 'Unable to send test notification: no Pushover App API token found in the global configuration.',
-		'pushover_error_nokey' => 'Unable to send test notification: no Pushover key found in your profile.',
-		'log_retention_period' => 'Log retention period',
-		'log_retention_period_description' => 'Number of days to keep logs of notifications and archives of server uptime. Enter 0 to disable log cleanup.',
+		'pushover_error' => 'Virhe Pushover-ilmoitusta lähetettäessä: %s',
+		'pushover_error_noapp' => 'Virhe lähetettäessä Pushover-ilmoitusta: Pushover API-avainta ei löydetty asetuksista.',
+		'pushover_error_nokey' => 'Virhe lähetettäessä Pushover-ilmoitusta: Pushover avainta ei löydetty profiilistasi.',
+		'log_retention_period' => 'Tapahtumien säilytysaika',
+		'log_retention_period_description' => 'Kuinka monta päivää pitää palvelinten päälläoloaikoja ja muita tapahtumia. Aseta arvoksi 0 jos haluat pitää ne loputtomasti.',
 		'log_retention_days' => 'päivää',
 	),
 	// for newlines in the email messages use <br/>
 	'notifications' => array(
-		'off_sms' => 'Server \'%LABEL%\' is DOWN: ip=%IP%, port=%PORT%. Error=%ERROR%',
-		'off_email_subject' => 'IMPORTANT: Server \'%LABEL%\' is DOWN',
-		'off_email_body' => "Failed to connect to the following server:<br/><br/>Server: %LABEL%<br/>IP: %IP%<br/>Port: %PORT%<br/>Error: %ERROR%<br/>Date: %DATE%",
-		'off_pushover_title' => 'Server \'%LABEL%\' is DOWN',
-		'off_pushover_message' => "Failed to connect to the following server:<br/><br/>Server: %LABEL%<br/>IP: %IP%<br/>Port: %PORT%<br/>Error: %ERROR%<br/>Date: %DATE%",
-		'on_sms' => 'Server \'%LABEL%\' is RUNNING: ip=%IP%, port=%PORT%',
-		'on_email_subject' => 'IMPORTANT: Server \'%LABEL%\' is RUNNING',
-		'on_email_body' => "Server '%LABEL%' is running again:<br/><br/>Server: %LABEL%<br/>IP: %IP%<br/>Port: %PORT%<br/>Date: %DATE%",
-		'on_pushover_title' => 'Server \'%LABEL%\' is RUNNING',
-		'on_pushover_message' => 'Server \'%LABEL%\' is running again:<br/><br/>Server: %LABEL%<br/>IP: %IP%<br/>Port: %PORT%<br/>Date: %DATE%',
+		'off_sms' => 'Palvelin \'%LABEL%\' on SAMMUNUT: ip=%IP%, portti=%PORT%. Virhe=%ERROR%',
+		'off_email_subject' => 'TÄRKEÄÄ: Palvelin \'%LABEL%\' on SAMMUNUT',
+		'off_email_body' => "Virhe yhdistettäessä seuraavaan palvelimeen:<br/><br/>Palvelin: %LABEL%<br/>IP: %IP%<br/>Portti: %PORT%<br/>Virhe: %ERROR%<br/>Aika: %DATE%",
+		'off_pushover_title' => 'Palvelin \'%LABEL%\' on sammunut',
+		'off_pushover_message' => "Virhe yhdistettäessä seuraavaan palvelimeen:<br/><br/>Palvelin: %LABEL%<br/>IP: %IP%<br/>Portti: %PORT%<br/>Virhe: %ERROR%<br/>Aika: %DATE%",
+		'on_sms' => 'Palvelin \'%LABEL%\' on PÄÄLLÄ: ip=%IP%, portti=%PORT%',
+		'on_email_subject' => 'TÄRKEÄÄ: Palvelin \'%LABEL%\' on PÄÄLLÄ',
+		'on_email_body' => "Palvelin '%LABEL%' on takaisin päällä:<br/><br/>PAlvelin: %LABEL%<br/>IP: %IP%<br/>Portti: %PORT%<br/>Aika: %DATE%",
+		'on_pushover_title' => 'Palvelin \'%LABEL%\' on PÄÄLLÄ',
+		'on_pushover_message' => 'Server \'%LABEL%\' on takaisin päällä:<br/><br/>Palvelin: %LABEL%<br/>IP: %IP%<br/>Portti: %PORT%<br/>Aika: %DATE%',
 	),
 	'login' => array(
 		'welcome_usermenu' => 'Tervetuloa, %user_name%',
