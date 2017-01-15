93dc1ca02ab9c9277a24b1ed8c0a219f6ad35ee0
Tom Hatzer <me@tomhatzer.at>
2017-01-15 14:42:03 +0100

    InstallController.php remove default db port

--- src/psm/Module/Install/Controller/InstallController.php
+++ src/psm/Module/Install/Controller/InstallController.php
@@ -121,7 +121,7 @@ class InstallController extends AbstractController {
 
 			$config = array(
 				'host' => 'localhost',
-				'port' => '3306',
+				'port' => '',
 				'name' => '',
 				'user' => '',
 				'pass' => '',
@@ -309,7 +309,7 @@ class InstallController extends AbstractController {
 			'pass' => '',
 			'name' => '',
 			'host' => '',
-			'port' => '3306'
+			'port' => ''
 		);
 		$pattern = "/define\('SM_DB_{key}', '(.*?)'/u";
 
