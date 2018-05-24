b64bc3af84ba75f414583b8a06a75275274ce367
Tim <TimZ99@users.noreply.github.com>
2018-05-24 09:26:06 +0200

    Changed checkbox to select input

--- src/psm/Util/Server/Updater/StatusUpdater.php
+++ src/psm/Util/Server/Updater/StatusUpdater.php
@@ -261,10 +261,13 @@ class StatusUpdater {
 				//Okay, the HTTP status is good : 2xx or 3xx. Now we have to test the pattern if it's set up
 				if($this->server['pattern'] != '') {
 					// Check to see if the body should not contain specified pattern
-					$pattern_online = !!$this->server['pattern_online'];
 					// Check to see if the pattern was [not] found.
-					if($pattern_online ? !preg_match("/{$this->server['pattern']}/i", $curl_result) : preg_match("/{$this->server['pattern']}/i", $curl_result)) {
-						$this->error = "TEXT ERROR : Pattern '{$this->server['pattern']}' " . ($pattern_online ? 'not' : 'was') . ' found.';
+					if($this->server['pattern_online'] == 'yes' ? 
+					   		preg_match("/{$this->server['pattern']}/i", $curl_result) : 
+					   		!preg_match("/{$this->server['pattern']}/i", $curl_result)) {
+						$this->error = "TEXT ERROR : Pattern '{$this->server['pattern']}' " . 
+							($this->server['pattern_online'] == 'yes' ? 'not' : 'was') . 
+							' found.';
 						$result = false;
 					}
 				}
