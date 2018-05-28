eb579e95407e4317b5001c2512154a0694fa8b07
Ward Pieters <19374765+wardpieters@users.noreply.github.com>
2018-05-28 16:15:32 +0200

    Update Smsit.php

--- src/psm/Txtmsg/Smsit.php
+++ src/psm/Txtmsg/Smsit.php
@@ -17,37 +17,31 @@
  * You should have received a copy of the GNU General Public License
  * along with PHP Server Monitor.  If not, see <http://www.gnu.org/licenses/>.
  *
- * @package     phpservermon
- * @author      nerdalertdk
- * @copyright   Copyright (c) 2008-2017 Pepijn Over <pep@mailbox.org>
- * @license     http://www.gnu.org/licenses/gpl.txt GNU GPL v3
- * @version     Release: @package_version@
- * @link        http://www.phpservermonitor.org/
- * @since       phpservermon 3.1
+ * @package		phpservermon
+ * @author		nerdalertdk
+ * @author		WardPieters
+ * @copyright	Copyright (c) 2008-2017 Pepijn Over <pep@mailbox.org>
+ * @license		http://www.gnu.org/licenses/gpl.txt GNU GPL v3
+ * @version		Release: @package_version@
+ * @link		http://www.phpservermonitor.org/
+ * @since		phpservermon 3.1
  **/
 
 namespace psm\Txtmsg;
 
 class Smsit extends Core {
-    // =========================================================================
-    // [ Fields ]
-    // =========================================================================
-    public $gateway         = 1;
-    public $resultcode      = null;
-    public $resultmessage   = null;
-    public $success         = false;
-    public $successcount    = 0;
-
-    public function sendSMS($message) {
-        // http://www.smsit.dk/api/sendSms.php?apiKey=[KEY]x&senderId=[SENDER]&mobile=[PHONENUMBER]&message=[MESSAGE]
-        // Use USERNAME as API KEY, password not needed
-        $apiurl     = "http://www.smsit.dk/api/sendSms.php";
-        $msg        = urlencode( $message );
-        $from       = urlencode( substr($this->originator,0,11) ); // Max 11 Char.
-        
-        foreach( $this->recipients as $phone ){
-            $URL = $apiurl."?apiKey=" . $this->username . "&mobile=" . $phone . "&message=" . $msg . "&senderId=" . $from;
-            $result = file_get_contents( $URL );
+	
+	public function sendSMS($message) {
+		$success = 0;
+		$error = "":
+		
+		// http://www.smsit.dk/api/sendSms.php?apiKey=[KEY]x&senderId=[SENDER]&mobile=[PHONENUMBER]&message=[MESSAGE]
+		// Use USERNAME as API KEY, password not needed
+		$API_URL = "https://www.smsit.dk/api/sendSms.php";
+		
+		foreach( $this->recipients as $phone ){
+			$URL = $API_URL."?apiKey=" . $this->username . "&mobile=" . $phone . "&message=" . urlencode($message) . "&senderId=" . $urlencode(substr($this->originator,0,11));
+			$result = file_get_contents($URL);
 			
 			/*
 				0	Everything went as it should
@@ -59,13 +53,14 @@ class Smsit extends Core {
 				6	The message is too long (That was she said)
 				7	API-key does not exist
 			*/
-			if((int)$result == 0) {
-				$success = true;
-			}
+			
+			if($result == "0") $success = 1;
+			else $error = $result;
+		
+		}
 
-        }
-
-        return $result;
-    }
+		if($success) return true;
+		else return $error;
+	}
 
 }
