f0a05e15b5d9bf2709ac6d876ef3b7dcee9fbf29
damian <damian@damian.id.au>
2018-08-19 16:43:31 +1000

    Updated puphet install script to include new config.php detail.

--- puphpet/files/exec-once/psminstall.sh
+++ puphpet/files/exec-once/psminstall.sh
@@ -7,6 +7,7 @@ define('PSM_DB_PASS', 'psm'); \
 define('PSM_DB_NAME', 'psm'); \
 define('PSM_DB_HOST', 'localhost'); \
 define('PSM_DB_PORT', ''); \
+define('PSM_THEME', 'default'); \
 ?>" > /var/www/default/psm/config.php
 
 echo "<?php \
