93d87274ef17f2c23c91eb04f408b3b33c1852b7
TimZ99 <TimZ99@users.noreply.github.com>
2020-05-17 23:16:59 +0200

    Display time in ms instead of seconds
    Only the template is effected, no db changes.

--- src/psm/Module/Server/Controller/AbstractServerController.php
+++ src/psm/Module/Server/Controller/AbstractServerController.php
@@ -114,7 +114,7 @@ abstract class AbstractServerController extends AbstractController
      */
     protected function formatServer($server)
     {
-        $server['rtime'] = round((float) $server['rtime'], 4);
+        $server['rtime'] = $server['rtime'];
         $server['last_online'] = psm_timespan($server['last_online']);
         $server['last_offline'] = psm_timespan($server['last_offline']);
         if ($server['last_offline'] != psm_get_lang('system', 'never')) {
