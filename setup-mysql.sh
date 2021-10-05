c18df60b2abb7f40522ab24856f1b01e93efde67
Ivan Iliev <ivan.iliev.iliev@gmail.com>
2021-10-05 20:13:40 +0300

    fixed: removed extra column at setup-mysql.sh script and changed access to robots.txt to deny all

--- setup-mysql.sh
+++ setup-mysql.sh
@@ -4,5 +4,5 @@ mysql -e "CREATE DATABASE psm;"
 # Create user
 mysql -e "CREATE USER 'psm'@'localhost' IDENTIFIED BY 'psm-dev-password';"
 mysql -e "GRANT ALL PRIVILEGES ON psm.* TO 'psm'@'localhost';"
-mysql -e "FLUSH PRIVILEGES;";
+mysql -e "FLUSH PRIVILEGES;"
 
