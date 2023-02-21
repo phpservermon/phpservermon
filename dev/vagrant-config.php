ec87f53522f423074b6af4bbe6aefad31acbd715
Jakub Kubień <kecalcze@gmail.com>
2023-02-21 13:24:55 +0100

    Add IP whitelist turn off option

--- dev/vagrant-config.php
+++ dev/vagrant-config.php
@@ -7,4 +7,5 @@ define('PSM_DB_HOST', 'localhost');
 define('PSM_DB_PORT', '3306'); //3306 is the default port for MySQL. If no specfic port is used, leave it empty.
 define('PSM_BASE_URL', '');
 define('PSM_WEBCRON_KEY', '');
+define('PSM_WEBCRON_ENABLE_IP_WHITELIST', 'true');
 define('PSM_PUBLIC', false);
