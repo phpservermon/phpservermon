3832dd75bbcc3f82dab86a6bb308f59274100622
taavityrnpuu <taavi.tyrnpuu@gmail.com>
2025-12-16 13:38:40 +0200

    Add explicit nullable parameter types

--- src/psm/Service/User.php
+++ src/psm/Service/User.php
@@ -87,9 +87,9 @@ class User
      * Open a new user service
      *
      * @param \psm\Service\Database $db
-     * @param \Symfony\Component\HttpFoundation\Session\SessionInterface $session if NULL, one will be created
+     * @param \Symfony\Component\HttpFoundation\Session\SessionInterface|null $session if NULL, one will be created
      */
-    public function __construct(Database $db, SessionInterface $session = null)
+    public function __construct(Database $db, ?SessionInterface $session = null)
     {
         $this->db_connection = $db->pdo();
 
